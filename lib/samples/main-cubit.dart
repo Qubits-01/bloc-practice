0ccf2369183de6883b3bfb455bc90d0d910300dc
Marc Peejay Viernes <mpvviernes@gmail.com>
2022-01-31 14:11:48 +0800

    Add sample cubit usage

--- lib/samples/main-cubit.dart
+++ lib/samples/main-cubit.dart
@@ -7,10 +7,34 @@ class CounterCubit extends Cubit<int> {
   void decrement() => emit(state - 1);
 }
 
-void main(List<String> args) {
+Future<void> main(List<String> args) async {
+  // final cubit = CounterCubit();
+  // print(cubit.state); //! Should print 0 = the default state of the counter.
+
+  // cubit.increment();
+  // print(cubit.state);
+
+  // cubit.increment();
+  // print(cubit.state);
+
+  // cubit.increment();
+  // print(cubit.state);
+
+  // cubit.decrement();
+  // print(cubit.state);
+
+  // cubit.close();
+
+  // Subscribe method
   final cubit = CounterCubit();
-  print(cubit.state); //! Should print 0 = the default state of the counter.
+  final subscription = cubit.stream.listen(print);
 
   cubit.increment();
-  print(cubit.state);
+  cubit.decrement();
+  cubit.increment();
+  cubit.increment();
+
+  await Future.delayed(Duration.zero);
+  await subscription.cancel();
+  await cubit.close();
 }
